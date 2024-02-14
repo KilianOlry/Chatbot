27dda35a3cc67c2ad42aa4a27fa2b77010f98906
kilian <kilian.olry@gmail.com>
2024-02-14 21:36:59 +0100

    feat(front) add image for user>Text

--- src/views/viewMessage.js
+++ src/views/viewMessage.js
@@ -1,12 +1,12 @@
 export default () => (`
 <div class="list-group">
 <a href="#" class="list-group-item list-group-item-action">
-  <div class="d-flex w-100 justify-content-between">
-    <h5 class="mb-1">List group item heading</h5>
-    <small class="text-body-secondary">3 days ago</small>
-  </div>
+  <div class="d-flex w-100 gap-2 align-items-center">
+  <img src="https://imgs.search.brave.com/9OLUzBkpzVC9rtgBkiOhA0IrfiAk8cDh7to3I7YugPQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2hhdGJvdC5jb20v/Y2hhdGJvdC1haS1h/c3Npc3QuZDk3ZmQ2/NzgxZDVhZGMwNTU1/MDQ2MTQ4NzMzZjJj/MDlmYjBhMjExNDI1/MTI3NmZjYjU3NWNl/ZjU3NjViMjVjMi5w/bmc" width='70' height='70' class="img-thumbnail border-0" alt="profileBot">
+    <h5 class="mb-1">Matéo grange</h5>
+    </div>
   <p class="mb-1">Some placeholder content in a paragraph.</p>
-  <small class="text-body-secondary">And some muted small print.</small>
+  <small class="text-body-secondary text-end">And some muted small print.</small>
 </a>
 </div>
 `);
