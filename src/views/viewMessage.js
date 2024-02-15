858a74ea89b97dd99247c685af151b24689facef
kilian <kilian.olry@gmail.com>
2024-02-15 15:56:41 +0100

    feat(front) add image cars

--- src/views/viewMessage.js
+++ src/views/viewMessage.js
@@ -1,12 +1,12 @@
 export default () => (`
-<div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action">
-    <div class="d-flex w-100 gap-2 align-items-center">
-      <img src="https://imgs.search.brave.com/9OLUzBkpzVC9rtgBkiOhA0IrfiAk8cDh7to3I7YugPQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2hhdGJvdC5jb20v/Y2hhdGJvdC1haS1h/c3Npc3QuZDk3ZmQ2/NzgxZDVhZGMwNTU1/MDQ2MTQ4NzMzZjJj/MDlmYjBhMjExNDI1/MTI3NmZjYjU3NWNl/ZjU3NjViMjVjMi5w/bmc" width='70' height='70' class="img-thumbnail border-0" alt="profileBot">
-      <h5 class="mb-1">Matéo grange</h5>
+<div class="message shadow-sm rounded-pill p-2">
+  <div class='d-flex align-items-center gap-2'>
+    <img src="https://imgs.search.brave.com/0VTo8VUZnbZd3RhVgdSh3ER2UrAB_SxA-Xlj_CiByec/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9mci50/b3B3b3JsZGF1dG8u/Y29tL3Bob3Rvcy8v/ZDcvYzcvbGlnaHRu/aW5nLW1jcXVlZW4t/cmVkLWNhci1mcm9t/LWRpc25leS0zOS1z/LW1vdmllLWNhcnMt/NjI1LmpwZw" width='80' class='inline-block rounded-circle shadow-sm' height='80' alt="">
+    <div>
+      <p class='d-block'>Flash McQueen</p>
+      <p>Le tonnerre arrive toujours après l'éclair</p>
+      <p class='text-end px-3 py-1 bg-primary d-inline text-white rounded-pill'>15/02/2024</p>
     </div>
-    <p class="mb-1">Some placeholder content in a paragraph.</p>
-    <small class="text-body-secondary text-end">And some muted small print.</small>
-  </a>
+  </div>
 </div>
 `);
