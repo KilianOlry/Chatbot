32658d4650414d397e521438ab76d70bc9072847
Matéo GRANGE <grange.mateo45@gmail.com>
2024-02-16 16:55:03 +0100

    feat <front>: quick fix view bots

--- src/models/Bot.js
+++ src/models/Bot.js
@@ -1,4 +1,12 @@
 const datas = [{
+  name: 'Thomas Shelby',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSD4hhYnxq3v-q05kUNHy6K-QDZFKsZM4cdJg&usqp=CAU',
+  actions: {
+    name: 'No Fighting. No F***ing Fighting, No Fighting, NO F*',
+    results: ''
+  }
+},
+{
   name: 'Flash McQueen',
   image: 'https://www.thedecofactory.com/pub/media/catalog/product/cache/9801f9953c8bc965c65fa73ccb3f71a9/r/m/rmk1518gm_lightning_mcqueen_giant_wall_decal_assembled_product.jpg',
   actions: {
--- src/views/viewBots.js
+++ src/views/viewBots.js
@@ -2,10 +2,10 @@ export default (botDatas) => (`
 <ul class="list-group list-group-flush botCard">
   ${botDatas.map((bot) => (
     `<li class="list-group-item d-flex align-items-center gap-2">
-      <img src="${bot.image}" alt="${bot.name}" width='80px' height='80px' class="img-thumbnail border-0" alt="profileBot">
+      <img src="${bot.image}" alt="${bot.name}" class='inline-block rounded-circle shadow-sm' height='80' width='80' height='80' class="img-thumbnail border-0" alt="profileBot">
       <div>
         <p class='fst-italic'>${bot.name}</p>
-        <span class='fst-italic'>${bot.actions.name}</span>
+        <span class='fst-italic d-inline-block text-truncate preview'>${bot.actions.name}</span>
       </div>
     </li>`
   )).join('')}
