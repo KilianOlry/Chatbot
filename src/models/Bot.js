c3e42670914f71f6dd014afacc2ed081269bb38c
kilian <kilian.olry@gmail.com>
2024-02-16 15:56:28 +0100

    feat(front): show all bot dynamic

---
+++ src/models/Bot.js
@@ -0,0 +1,27 @@
+const datas = [{
+  name: 'Flash McQueen',
+  image: 'https://www.thedecofactory.com/pub/media/catalog/product/cache/9801f9953c8bc965c65fa73ccb3f71a9/r/m/rmk1518gm_lightning_mcqueen_giant_wall_decal_assembled_product.jpg',
+  actions: {
+    name: 'Cars',
+    results: ''
+  }
+},
+{
+  name: 'Louis Bodin',
+  image: 'https://voi.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fvoi.2Fvar.2Fvoi.2Fstorage.2Fimages.2Fmedia.2Fmultimedia.2Flouis-bodin-revient-sur-le-sketch-des-guignols-suite-au-drame-de-dropped.2F9538354-1-fre-FR.2Flouis-bodin-revient-sur-le-sketch-des-guignols-suite-au-drame-de-dropped.2Ejpg/170x170/quality/80/video-louis-bodin-revient-sur-le-sketch-des-guignols-suite-au-drame-de-dropped.jpg',
+  actions: {
+    name: 'Meteo',
+    results: ''
+  }
+},
+{
+  name: 'Stéphane Bern',
+  image: 'https://www.gala.fr/imgre/fit/~1~gal~2023~06~30~1730db8f-85d9-4b7f-96de-df5a48813f07.png/150x150/quality/80/stephane-bern-son-havre-de-paix-loin-de-paris.jpg',
+  actions: {
+    name: 'City',
+    results: ''
+  }
+}
+];
+
+export default datas;
--- src/views/viewBots.js
+++ src/views/viewBots.js
@@ -1,11 +1,13 @@
-export default () => (`
+export default (botDatas) => (`
 <ul class="list-group list-group-flush botCard">
-  <li class="list-group-item d-flex align-items-center gap-2">
-    <img src="https://www.thedecofactory.com/pub/media/catalog/product/cache/9801f9953c8bc965c65fa73ccb3f71a9/r/m/rmk1518gm_lightning_mcqueen_giant_wall_decal_assembled_product.jpg" width='80' height='80' class="img-thumbnail border-0" alt="profileBot">
-    <div>
-      <p class='fst-italic'>Matéo Grange</p>
-      <span class='fst-italic'>C'est chibrer !!!</span>
-    </div>
-  </li>
+  ${botDatas.map((bot) => (
+    `<li class="list-group-item d-flex align-items-center gap-2">
+      <img src="${bot.image}" alt="${bot.name}" width='80px' height='80px' class="img-thumbnail border-0" alt="profileBot">
+      <div>
+        <p class='fst-italic'>${bot.name}</p>
+        <span class='fst-italic'>${bot.actions.name}</span>
+      </div>
+    </li>`
+  )).join('')}
 </ul>
 `);
