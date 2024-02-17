0654e6ff286d3f8507431070493ae1f03671aaa4
kilian <kilian.olry@gmail.com>
2024-02-17 09:31:58 +0100

    feat(header): index.scss / nav.js

--- src/views/nav.js
+++ src/views/nav.js
@@ -1,11 +1,11 @@
 export default () => (`
-<nav class="navbar border-bottom">
-  <div class="container-fluid py-1">
-    <a class="navbar-brand d-flex align-items-center gap-3 fs-3" href="#">
+<nav class="container__navbar">
+  <div class="container container__navbar__navbar">
+    <a class="container__navbar__navbar__logo" href="#">
       <img src="https://imgs.search.brave.com/9OLUzBkpzVC9rtgBkiOhA0IrfiAk8cDh7to3I7YugPQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2hhdGJvdC5jb20v/Y2hhdGJvdC1haS1h/c3Npc3QuZDk3ZmQ2/NzgxZDVhZGMwNTU1/MDQ2MTQ4NzMzZjJj/MDlmYjBhMjExNDI1/MTI3NmZjYjU3NWNl/ZjU3NjViMjVjMi5w/bmc" alt="Logo" width="80" height="80" class="d-inline-block align-text-top">
       ChatBot
     </a>
-    <i class="ri-menu-3-line fs-2 toggleBtn"></i>
+    <i class="ri-menu-3-line fs-2 toggleBtn container__navbar__toggle"></i>
   </div>
 </nav> 
 `);
