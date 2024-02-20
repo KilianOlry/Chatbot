4a8813a27dba9eb4d50dd139ac3097cf743a2b20
kilian <kilian.olry@gmail.com>
2024-02-20 15:01:39 +0100

    refactor(item-bot.js): abstract item bot

--- src/views/chatBot/viewBots.js
+++ src/views/chatBot/viewBots.js
@@ -1,3 +1,5 @@
+import itemBot from './item-bot';
+
 export default (botDatas) => (`
 <ul class="bot__list">
   <li class='bot__list__item galery'>
@@ -5,14 +7,6 @@ export default (botDatas) => (`
       <i class="ri-gallery-view-2"></i>
     </a>
   </li>
-  ${botDatas.map((bot) => (
-    `<li class="bot__list__item" >
-      <img src="${bot.image}" alt="${bot.name}" class='' height='80' width='80' height='80' class="img-thumbnail border-0" alt="profileBot">
-      <div class='__description'>
-        <p class='__description__bot__name'>${bot.name}</p>
-        <span class='__description__bot__message'>${bot.actions.name}</span>
-      </div>
-    </li>`
-  )).join('')}
+    ${itemBot(botDatas)}
 </ul>
 `);
