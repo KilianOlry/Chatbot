d0ea2ded12048babbbe4762d70390fb28ca0437b
kilian <kilian.olry@gmail.com>
2024-04-07 16:26:26 +0200

    feat(Bots.js / list-bot / viewBots): remove folder image / call api for display bots in page BotsPage

--- src/controllers/Bots.js
+++ src/controllers/Bots.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import viewNav from '../views/nav';
 import listBots from '../views/bots/list-bots';
 
@@ -9,12 +10,12 @@ const Bots = class {
     this.run();
   }
 
-  renderSkeleton() {
+  async renderSkeleton() {
     return `
       ${viewNav()}
       <h1 class= 'title-bot-page'>Nos <span>Bots</span></h1>
       <main class='main-listBots'>
-        ${listBots()}
+        ${listBots(await this.botsData())}
       </main>
     `;
   }
@@ -27,8 +28,18 @@ const Bots = class {
     });
   }
 
-  run() {
-    this.el.innerHTML = this.renderSkeleton();
+  async botsData() {
+    const apiUrl = 'http://localhost/bots';
+    try {
+      const response = await axios.get(apiUrl);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async run() {
+    this.el.innerHTML = await this.renderSkeleton();
     this.toggleBtn();
   }
 };
--- src/views/bots/bot.js
+++ src/views/bots/bot.js
@@ -1,14 +1,14 @@
 export default (data) => {
-  const { name, image, actions } = data;
+  const { name, images, actions } = data;
   return `
   <article class='card-bot'>
-    <div class='img-bot' style="background-image: url('${image}')"></div>
+    <div class='img-bot' style="background-image: url('${images}')"></div>
     <div class='content-text'>
       <ul>
       <li class='botName'>${name}</li>
         <li class='items'>Actions :
           <ul class='item items'>
-            <li>${actions.name}</li>
+            <li>${actions}</li>
           </ul>
         </li>
       </ul>
